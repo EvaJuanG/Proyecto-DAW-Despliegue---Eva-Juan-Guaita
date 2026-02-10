7c89d5ae53a1b53ad80a873c262258b5a9086ac5
Eva <evajuagua@alu.edu.gva.es>
2026-02-10 22:26:57 +0100

    Configuro urls.js para que detecte el servidor automáticamente

--- frontend/src/public_html/config/urls.js
+++ frontend/src/public_html/config/urls.js
@@ -1,5 +1,12 @@
-//Rutas de las llamadas HTTP
-//Rutas de las llamadas HTTP
-let apiUrl = (window.location.hostname.includes('localhost') || window.location.hostname.includes('127.0.0.1'))
-    ? "http://" + window.location.hostname + ":8080"
-    : "http://" + window.location.hostname + ":8080"; // Fallback for other local addresses
+let apiUrl = "";
+
+if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
+    // Si estás en tu PC local
+    apiUrl = "http://localhost:8080";
+} else if (window.location.hostname.includes('render.com')) {
+    // Si estás visualizando la web desde Render
+    apiUrl = "https://pelis-backend.onrender.com";
+} else {
+    // Si estás en AWS (usando tu dominio de chickenkiller)
+    apiUrl = "http://" + window.location.hostname + ":8080";
+}
